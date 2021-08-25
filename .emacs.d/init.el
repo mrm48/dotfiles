899b031c486799fbde50f281e4a23a25a12d6e63
Matt Miller <mmiller@crispyecho.com>
2021-08-25 07:26:44 -0400

    Adding in org-roam-dailies configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -641,12 +641,19 @@
   :ensure t
   :init
   (setq org-roam-v2-ack t)
+  (setq org-roam-dailies-directory "daily/")
   :custom
   (org-roam-directory "~/vger/Documents/RoamNotes")
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n i" . org-roam-node-insert)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
   :config
+  (require 'org-roam-dailies)
   (org-roam-setup))
 
 (use-package lsp-jedi

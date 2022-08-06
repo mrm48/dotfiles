76213c881e8899ee539d5b3290fe96456eacea62
Matt Miller <mmiller@crispyecho.com>
2022-08-06 06:00:47 -0400

    Update nvim lua config - theme, lua, plugins
    Update personal nvim config to use tokyo night and use gopls for golang lsp

--- .config/nvim/lua/matt.lua
+++ .config/nvim/lua/matt.lua
@@ -1,1 +1,13 @@
 vim.opt.cursorline = true
+
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transparent = true
+
+-- run in go in a new split so don't have to switch focus
+-- function RunGoInNewSplit(args)
+--vim.api.nvim_command('split')
+-- don't like this solution
+--vim.api.nvim_command('terminal')
+--
+--go run .
+--end

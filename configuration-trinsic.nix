a57f9d03256154b031c2b23da03cec91f2f511ca
Matt Miller <mmiller@crispyecho.com>
2023-12-12 16:28:07 -0500

    Switch from plasma to gnome on trinsic

--- configuration-trinsic.nix
+++ configuration-trinsic.nix
@@ -45,12 +45,12 @@
   services.flatpak.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Enable GNOME
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {

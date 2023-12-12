148b3d553f44d77634c4c2929f1bedf4e66be922
Matt Miller <mmiller@crispyecho.com>
2023-12-12 06:38:46 -0500

    Add in settings for switching to GNOME - trinsic

--- configuration-trinsic.nix
+++ configuration-trinsic.nix
@@ -53,6 +53,10 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+  # Enable GNOME
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
   # Configure keymap in X11
   services.xserver = {
     layout = "us";

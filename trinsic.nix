26b503a7bc75d0c8bc600a1ca85eb2abc3ef6b82
Matt Miller <mmiller@crispyecho.com>
2023-12-12 16:51:53 -0500

    Add in blur-my-shell

--- trinsic.nix
+++ trinsic.nix
@@ -12,6 +12,7 @@
     pkgs.gnome.gnome-boxes
     pkgs.gnome.gnome-tweaks
     pkgs.chrome-gnome-shell
+    pkgs.gnomeExtensions.blur-my-shell
     pkgs.gnomeExtensions.dash-to-dock
     pkgs.gnomeExtensions.dash-to-panel
     pkgs.gnomeExtensions.caffeine

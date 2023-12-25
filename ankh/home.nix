ad11a5b4e9e8719395f9c689407474c27a02fac5
Matt Miller <mmiller@crispyecho.com>
2023-12-25 06:05:10 -0500

    Add mediawriter to ankh for burning isos

--- ankh/home.nix
+++ ankh/home.nix
@@ -21,6 +21,9 @@
     #game dev testing
     godot_4
 
+    #tools
+    mediawriter
+
     (writeShellScriptBin "setup-favorites" ''
       # Set up favorites for gnome convert this to an autostart script
       gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop', 'com.obsproject.Studio.desktop', 'brave-browser.desktop', 'freetube.desktop']"

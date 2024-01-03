64c08a2975f9dcaaf3ac322416d24e375584322b
Matt Miller <mmiller@crispyecho.com>
2024-01-03 10:48:11 -0500

    Add in wofi stylesheet

--- ankh/home.nix
+++ ankh/home.nix
@@ -32,11 +32,72 @@
 
     #hyprland configuration stuff
     wofi
+    hyprpaper
 
     (writeShellScriptBin "setup-favorites" ''
       # Set up favorites for gnome convert this to an autostart script
       gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop', 'com.obsproject.Studio.desktop', 'steam.desktop', 'freetube.desktop', 'brave-browser.desktop', 'net.lutris.Lutris.desktop' ]"
-    '')
-  ];
+      '')
+
+    ];
+
+
+      programs.wofi = {
+        enable = true;
+        style = ''
+
+          window {
+          margin: 0px;
+          border: 1px solid #bd93f9;
+          background-color: #282a36;
+          }
+          
+          #input {
+          margin: 5px;
+          border: none;
+          color: #f8f8f2;
+          background-color: #44475a;
+          }
+          
+          #inner-box {
+          margin: 5px;
+          border: none;
+          background-color: #282a36;
+          }
+          
+          #outer-box {
+          margin: 5px;
+          border: none;
+          background-color: #282a36;
+          }
+          
+          #scroll {
+          margin: 0px;
+          border: none;
+          }
+          
+          #text {
+          margin: 5px;
+          border: none;
+          color: #f8f8f2;
+          } 
+          
+          #entry.activatable #text {
+          color: #282a36;
+          }
+          
+          #entry > * {
+          color: #f8f8f2;
+          }
+          
+          #entry:selected {
+          background-color: #44475a;
+          }
+          
+          #entry:selected #text {
+          font-weight: bold;
+          }
+        '';
+    };
 
 }

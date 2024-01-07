d9734068e9cd40c96079bd04f686c0f9f8635228
Matt Miller <mmiller@crispyecho.com>
2024-01-07 12:57:21 -0500

    Add in virt-manager for trinsic

--- trinsic/configuration.nix
+++ trinsic/configuration.nix
@@ -11,6 +11,10 @@
       ../modules/nvidia.nix 
     ];
 
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+  security.polkit.enable = true;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -18,5 +22,7 @@
   boot.initrd.luks.devices."luks-e7e0d04a-32c6-46bd-a7f1-e40c4479ddc6".device = "/dev/disk/by-uuid/e7e0d04a-32c6-46bd-a7f1-e40c4479ddc6";
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "hid_generic" "hid_asus" ];
   networking.hostName = "trinsic";
+
+  users.users.matt.extraGroups = [ "libvirtd" ];
 
 }

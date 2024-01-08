2ccfeabd2525ea25e64046c54edaa26d42290785
Matt Miller <mmiller@crispyecho.com>
2024-01-08 14:38:18 -0500

    move neovim config to nixvim

--- modules/neovim/init.nix
+++ modules/neovim/init.nix
@@ -1,47 +1,18 @@
-{ config, pkgs, ... }:
-{
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-      nvim-jdtls
-      mason-nvim
-      mason-lspconfig-nvim
-      nvim-treesitter
-      telescope-nvim
-      plenary-nvim
+{ config, pkgs, lib, ... }:
+let
+  nixvim = import (builtins.fetchGit {
+    url = "https://github.com/nix-community/nixvim";
+  });
+in
+  {
+    imports = [
+      nixvim.homeManagerModules.nixvim
     ];
-    extraConfig = ''
-      luafile /home/matt/.dotfiles/modules/neovim/globalopts.lua
-      luafile /home/matt/.dotfiles/modules/neovim/set.lua
-    '';
-    extraLuaConfig = ''
-        local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
-        vim.fn.mkdir(parser_install_dir, "p")
-        -- Prevents reinstall of treesitter plugins every boot
-        vim.opt.runtimepath:append(parser_install_dir)
 
-        require'nvim-treesitter.configs'.setup {
-          ensure_installed = "all",
-          parser_install_dir = parser_install_dir, 
-          highlight = {
-            enable = true,
-          },
-        }
-
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-        require("mason").setup()
-        require("mason-lspconfig").setup {
-          automatic_installation = true,
-        }
-    '';
+  programs.nixvim = {
+      enable = true;
+      colorschemes.gruvbox.enable = true;
+      plugins.lightline.enable = true;
   };
 
   home.sessionVariables = {

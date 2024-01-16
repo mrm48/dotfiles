de9ad080c37be2d34264abb1983dfe8fb55dcbf0
Matt Miller <mmiller@crispyecho.com>
2024-01-15 22:44:31 -0500

    Change nvim from nord to gruvbox

--- modules/neovim/init.nix
+++ modules/neovim/init.nix
@@ -13,6 +13,7 @@
       telescope-nvim
       plenary-nvim
       nord-nvim
+      gruvbox-nvim
       lualine-nvim
       harpoon
       bufferline-nvim
@@ -56,20 +57,35 @@
 
         vim.keymap.set('n', '<leader>ha', ui.toggle_quick_menu)
 
-        vim.g.nord_contrast = true
-        vim.g.nord_borders = false
-        vim.g.nord_disable_background = true
-        vim.g.nord_italic = false
-        vim.g.uniform_diff_background = false
-        vim.g.nord_bold = false
-
-        require('nord').set()
-
-        vim.cmd[[colorscheme nord]]
+        require("gruvbox").setup({
+            terminal_colors = true, -- add neovim terminal colors
+            undercurl = true,
+            underline = true,
+            bold = true,
+            italic = {
+              strings = true,
+              emphasis = true,
+              comments = true,
+              operators = false,
+              folds = true,
+            },
+            strikethrough = true,
+            invert_selection = false,
+            invert_signs = false,
+            invert_tabline = false,
+            invert_intend_guides = false,
+            inverse = true, -- invert background for search, diffs, statuslines and errors
+            contrast = "", -- can be "hard", "soft" or empty string
+            palette_overrides = {},
+            overrides = {},
+            dim_inactive = false,
+            transparent_mode = false,
+        })
+        vim.cmd("colorscheme gruvbox")
 
         require('lualine').setup {
           options = {
-            theme = 'nord'
+            theme = 'gruvbox'
           }
         }
 

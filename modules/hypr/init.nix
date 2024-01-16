0f0ace3456c2df50ee3c76885efba7dd38149cd9
Matt Miller <mmiller@crispyecho.com>
2024-01-15 22:45:03 -0500

    Change active border to gruvbox red

--- modules/hypr/init.nix
+++ modules/hypr/init.nix
@@ -31,7 +31,7 @@
         gaps_in = 5;
         gaps_out = 5;
         border_size = 2;
-        "col.active_border" = "rgba(5e81acee) rgba(5e81acee) 45deg";
+        "col.active_border" = "rgba(ea6962ee) rgba(ea6962ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
   
         layout = "master";

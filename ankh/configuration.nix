d878ab4d05ad74198a8fb09d777b64db5eb94086
Matt Miller <mmiller@crispyecho.com>
2024-01-18 07:16:42 -0500

    Switch from gnome to plasma

--- ankh/configuration.nix
+++ ankh/configuration.nix
@@ -7,8 +7,8 @@
 {
 
   # Enable the Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma.enable = true;
 
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
--- ankh/home.nix
+++ ankh/home.nix
@@ -12,16 +12,6 @@
 
   home.packages = with pkgs; [
 
-    #gnome extensions
-    gnome.gnome-boxes
-    gnome.gnome-tweaks
-    chrome-gnome-shell
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.dash-to-panel
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.caffeine
-
     #social
     obs-studio
 
@@ -33,11 +23,6 @@
 
     #games
     protonup-qt
-
-    (writeShellScriptBin "setup-favorites" ''
-      # Set up favorites for gnome convert this to an autostart script
-      gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop', 'com.obsproject.Studio.desktop', 'steam.desktop', 'freetube.desktop', 'net.lutris.Lutris.desktop' ]"
-      '')
 
     ];
 

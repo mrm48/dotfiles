478f9e7c9055d832f4ecec38c130472be90e294e
Matt Miller <mmiller@crispyecho.com>
2024-01-25 18:15:59 -0500

    Remove flatpak
    Top level configuration does not need flatpak, only needed for ankh testing (rarely)

--- configuration.nix
+++ configuration.nix
@@ -35,8 +35,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  services.flatpak.enable = true;
-
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;

19f94b553757024b2f2aefe90291536244eb7c43
Matt Miller <mmiller@crispyecho.com>
2024-03-01 13:02:50 -0500

    Add back in blesh after resolving ver variable

--- modules/console/default.nix
+++ modules/console/default.nix
@@ -67,6 +67,9 @@
       if ! { [ "$TERM" = "screen-256color" ] && [ -n "$TMUX" ]; } then
         launchtmux
       fi
+      source "$(blesh-share)"/ble.sh --attach=none
+      ble-attach
+      eval "$(atuin init bash)"
 	'';
   };
 

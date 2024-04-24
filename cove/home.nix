34d6f7c36be5825d3ebbee36bda38d6d0bb590b3
Matt Miller <mmiller@crispyecho.com>
2024-04-24 08:34:01 -0400

    Configure home-manager to switch back to 23.11

--- cove/home.nix
+++ cove/home.nix
@@ -3,12 +3,12 @@
 
 {
 
-  # hyprland configuration
-  imports = [
+  # sway configuration
+  #imports = [
 
-   ../modules/hypr
+   #../modules/sway
 
-  ]; 
+  #];
 
   home.packages = with pkgs; [
 
@@ -18,35 +18,15 @@
 
     #tools
     mediawriter
-    kdePackages.kate
+    kate
     librewolf
+    vivaldi
+    vivaldi-ffmpeg-codecs
     logseq
 
     waybar
     networkmanagerapplet
 
   ];
-
-  wayland.windowManager.hyprland.settings = {
-      monitor= [ "DP-1,1920x1080@144,0x0,1"
-                 "HDMI-A-1,1920x1080@60,1920x0,1"
-      ];
-      misc = {
-        # See https://wiki.hyprland.org/Configuring/Variables/ for more
-        force_default_wallpaper = 0; # Set to 0 to disable the anime mascot wallpapers
-      };
-      exec-once = [ 
-        "hyprpaper"
-        "waybar"
-        "nm-applet --indicator"
-      ];
-  };
-
-  home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = /home/matt/Pictures/walls/wallpaper
-
-    wallpaper = DP-1,/home/matt/Pictures/walls/wallpaper
-    wallpaper = HDMI-A-1,/home/matt/Pictures/walls/wallpaper
-  '';
 
 }

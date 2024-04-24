c039d56a137d8d59c020aa8e1776524234c79fae
Matt Miller <mmiller@crispyecho.com>
2024-04-24 08:47:32 -0400

    Remove Qt6 version of kclock

--- home.nix
+++ home.nix
@@ -29,18 +29,9 @@
       keepassxc
       gimp
       inkscape
-      brave
-      kdePackages.kclock
+      libsForQt5.kclock
       mpv
       vlc
-      streamlink
-      gst_all_1.gstreamer
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-      gst_all_1.gst-plugins-bad
-      gst_all_1.gst-plugins-ugly
-      gst_all_1.gst-libav
-      gst_all_1.gst-vaapi
 
       #social
       freetube

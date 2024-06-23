b30c40534b61652830dfd86c8084ca471fcd95aa
Matt Miller <mmiller@crispyecho.com>
2024-06-23 08:04:08 -0400

    Fix clock format

--- modules/hypr/default.nix
+++ modules/hypr/default.nix
@@ -235,7 +235,7 @@
         modules-right = [ "battery" "wireplumber" "tray" ];
         clock = {
           interval = 60;
-          format = "{:%H:%M}";
+          format = "{:%a :%b :%I:%M :%P}";
           max-length = 25;
         };
       };

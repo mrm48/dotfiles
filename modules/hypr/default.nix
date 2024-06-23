1fa07173ea2d06d64730fecee964f9d91d0856bb
Matt Miller <mmiller@crispyecho.com>
2024-06-23 07:59:27 -0400

    Test clock with just the day and month

--- modules/hypr/default.nix
+++ modules/hypr/default.nix
@@ -235,7 +235,7 @@
         modules-right = [ "battery" "wireplumber" "tray" ];
         clock = {
           interval = 60;
-          format = "{%a %b %I:%M %p}";
+          format = "{%a %b}";
           max-length = 25;
         };
       };
